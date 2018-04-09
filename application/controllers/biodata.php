dd87738f04b75491f46734c7213dcfe6213ad913
Revina Laksmi <revinaaniver@gmail.com>
2018-04-09 21:22:22 +0700

    tugas3

--- application/controllers/biodata.php
+++ application/controllers/biodata.php
@@ -5,19 +5,13 @@ class Biodata extends CI_Controller {
 	
 	public function index(){
 		$this->load->model('mymodel');
-
 		$data['result'] = $this->mymodel->GetArtikel();
-
 		$this->load->view('home2', $data);
 	} 
 
 	public function do_preview($id=''){
 		$this->load->model('mymodel');
-
 		$data['isi'] = $this->mymodel->GetPreview($id);
-
-		
-
 		$this->load->view('preview', $data);
 	}
 
@@ -44,60 +38,29 @@ class Biodata extends CI_Controller {
         {
             $data = array('upload_data' => $this->upload->data());
 
-            $id				= $_POST['id'];
             $judul 			= $_POST['judul'];
 			$penyanyi 		= $_POST['penyanyi'];
 			$tahun_rilis	= date("Y-m-d H:i:s");
 			$deskripsi 		= $_POST['deskripsi'];
-			$img			= $this->upload->data('file_name');
+			$gambar		= $this->upload->data('file_name');
 			
 			$data_insert	= array(
-									'id' 	=> $id,
 									'judul'		=> $judul,
 									'penyanyi'	=> $penyanyi,
 									'tahun_rilis' 		=> $tahun_rilis,
 									'deskripsi' 		=> $deskripsi,
-									'img'		=> $gambar
+									'gambar'		=> $gambar
 								);
 
 			$this->load->model('mymodel');
-			$res = $this->mymodel->InsertData('artikel', $data_insert);
+			$res = $this->mymodel->InsertData('biodata', $data_insert);
 			
 			if($res>=1){
 				$this->session->set_flashdata('pesan','Tambah Data Sukses');
-				redirect('crud/index');
+				redirect('biodata/home2');
 			}else{
 				echo "<h2>Insert Data Gagal</h2>";	
 			}
         }
 	}
-	
-	public function upload(){
-	 	$this->load->view('upload_form', array('error' => ' ' ));
-	}
-
-	public function do_upload()
-        {
-                $config['upload_path']          = 'assets/img/';
-                $config['allowed_types']        = 'gif|jpg|png';
-                $config['max_size']             = 100;
-                $config['max_width']            = 1024;
-                $config['max_height']           = 768;
-
-                $this->load->library('upload', $config);
-
-                if ( ! $this->upload->do_upload('userfile'))
-                {
-                        $error = array('error' => $this->upload->display_errors());
-
-                        $this->load->view('upload_form', $error);
-                }
-                else
-                {
-                        $data = array('upload_data' => $this->upload->data());
-
-                        $this->load->view('sukses', $data);
-                }
-        }
-	
 }
